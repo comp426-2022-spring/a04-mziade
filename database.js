976045e7baedafb68e17a3cae4d07dd8e6983198
Mia Ziade <mziade@live.unc.edu>
2022-04-11 15:45:32 -0400

    update

--- database.js
+++ database.js
@@ -17,8 +17,7 @@ if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        CREATE TABLE accesslog ( remoteadr: TEXT, remoteuser: TEXT, time: TEXT, method: TEXT, url: TEXT, protocal: TEXT,
-            httpversion: TEXT, status: TEXT, referer: TEXT,  useragent: TEXT);
+        CREATE TABLE accesslog ( remoteaddr: TEXT, remoteuser: TEXT, time: TEXT, method: TEXT, url: TEXT, protocol: TEXT, httpversion: TEXT, status: TEXT, referer: TEXT,  useragent: TEXT);
    `;
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.use("/app/new/user", (req, res, next) => {
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
     }
-    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocal, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocal, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
     res.status(200).json(info)
 	next()
 })
