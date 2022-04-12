f9458bf45e2ae76839fb27d5e322329059517617
Mia Ziade <mziade@live.unc.edu>
2022-04-12 14:57:12 -0400

    moved location of next

--- server.js
+++ server.js
@@ -65,9 +65,9 @@ const myFunc = function(req, res, next) {
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
     };
-    next()
     const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
     const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    next()
     // res.status(200).json(info)
 };
 
