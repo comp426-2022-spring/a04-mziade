9a0898c52be74400335f90e532f358917c482394
Mia Ziade <mziade@live.unc.edu>
2022-04-12 21:05:45 -0400

    id

--- server.js
+++ server.js
@@ -54,7 +54,6 @@ app.get("/app/", (req, res, next) => {
 // Middleware
 const myFunc = function(req, res, next) {
 	let logdata = {
-        id: req.id,
         remoteaddr: req.ip,
         remoteuser: req.user,
         time: Date.now(),
@@ -66,8 +65,8 @@ const myFunc = function(req, res, next) {
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
     };
-    const stmt = db.prepare('INSERT INTO accesslog (id, remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    const info = stmt.run(logdata.id, logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
     next()
     // res.status(200).json(info)
 };
